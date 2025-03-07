d4a954d7a4d891d5b5109f4778bc526fb15a79bf
Fatih Alperen  Bolat <125909061+ElectronicEngineerr@users.noreply.github.com>
2025-03-08 01:54:00 +0300

    Add files via upload

--- VHDL FILES/tb_UART_RX.vhd
+++ VHDL FILES/tb_UART_RX.vhd
@@ -8,7 +8,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 entity tb_UART_RX is
 			generic (
 						c_clock_freq   : integer := 100_000_000;
-						c_baud_rate    : integer := 10_000_000
+						c_baud_rate    : integer := 115_200
 			);
 end tb_UART_RX;
 
@@ -18,7 +18,7 @@ architecture Behavioral of tb_UART_RX is
 COMPONENT UART_RX is
 			generic (
 						c_clock_freq   : integer := 100_000_000;
-						c_baud_rate    : integer := 10_000_000
+						c_baud_rate    : integer := 115_200
 			);
 			port(
 						clk 		     : in  std_logic;
@@ -28,16 +28,17 @@ COMPONENT UART_RX is
 			);
 end COMPONENT;
 
-signal clk 		        	: std_logic := '0';
-signal rx_input_data_i  	: std_logic := '1';
-signal rx_done_tick_o   	: std_logic := '0';
-signal rx_data_output_o 	: std_logic_vector(7 downto 0) := (others => '0');
+signal clk 		        		: std_logic := '0';
+signal rx_input_data_i  		: std_logic := '1';
+signal rx_done_tick_o   		: std_logic := '0';
+signal rx_data_output_o 		: std_logic_vector(7 downto 0) := (others => '0');
+	
+signal clock_period 			: time := 10 ns;
+signal baud_time 				: time := 8.68 us;
+constant example_tx_data_hex_AA	: std_logic_vector(9 downto 0) := '1' & x"FF" & '0';
+constant example_tx_data_hex_45	: std_logic_vector(9 downto 0) := '1' & x"45" & '0';
+constant example_tx_data_hex_BA	: std_logic_vector(9 downto 0) := '1' & x"BA" & '0';
 
-signal clock_period 		: time := 10 ns;
-signal c_baud_time			: time := 100 ns;
-constant example_hex_c1		: std_logic_vector(9 downto 0) := '1' & x"AA" & '0';
-constant example_hex_c2		: std_logic_vector(9 downto 0) := '1' & x"13" & '0';
-constant example_hex_c3		: std_logic_vector(9 downto 0) := '1' & x"5B" & '0';
 
 begin
 
@@ -67,32 +68,29 @@ end process CLOCK_GENERATOR;
 
 P_STIMULI : process
 	begin
-			wait for 100 ns;
-			for i in 0 to 9 loop
-					rx_input_data_i <= example_hex_c1(i);
-					wait for c_baud_time;
-			end loop;
-			
-			wait for c_baud_time;
-			
-			wait for 100 ns;
-			for i in 0 to 9 loop
-					rx_input_data_i <= example_hex_c2(i);
-					wait for c_baud_time;
-			end loop;
-			
-			wait for c_baud_time;
-			
-			wait for 100 ns;
-			for i in 0 to 9 loop
-					rx_input_data_i <= example_hex_c3(i);
-					wait for c_baud_time;
-			end loop;
-			
-			
-			assert false
-			report "SIM DONE"
-			severity failure;
+				
+				wait for clock_period*10;
+				
+				for i in 0 to 9 loop
+					rx_input_data_i <= example_tx_data_hex_AA(i);
+					wait for baud_time;
+				end loop;
+				wait for baud_time;
+				
+				for i in 0 to 9 loop
+					rx_input_data_i <= example_tx_data_hex_45(i);
+					wait for baud_time;
+				end loop;
+				
+				wait for baud_time;
+				
+				for i in 0 to 9 loop
+					rx_input_data_i <= example_tx_data_hex_BA(i);
+					wait for baud_time;
+				end loop;
+				
+				wait for baud_time;
+
 	
 		
 end process P_STIMULI;
